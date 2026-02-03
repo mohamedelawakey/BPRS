c1c6ba1c4096dc02484896bc27f194ba4adbbe20
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-03 22:22:09 +0200

    update: update and add some queries and number of books

--- ml/Enum/Enumerations.py
+++ ml/Enum/Enumerations.py
@@ -7,3 +7,23 @@ class Enumerations():
     pattern_punctuation_lines_repeated = r'[-_*/]{2,}'
     non_printing_control_characters = r'[^\x00-\x7F]+'
     useless_symbols_and_signs = r'[^\w\s]'
+
+    # SQL Queries
+    vector_service_query = """
+        SELECT book_id, 1 - (embedding <#> %s ::vector) AS similarity
+        FROM books
+        ORDER BY embedding <#> %s ::vector
+        LIMIT %s
+    """
+
+    metadata_service_query = """
+        SELECT book_id, name, name_cleaned, authors, publisher, description,
+        description_cleaned, rating, publishyear, weighted_rating,
+        counts_of_review_scaled, tech_score_scaled, publishyear_scaled,
+        average_low_rating, average_high_rating
+        FROM books
+        WHERE book_id = ANY(%s)
+    """
+
+    # books
+    top_k = 100
