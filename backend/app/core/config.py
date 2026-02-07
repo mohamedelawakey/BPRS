05beb37de0e721158f1e8b7ba9bb72c41f449e53
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-07 03:14:38 +0200

    update: add the redis database configrations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,3 +8,8 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
