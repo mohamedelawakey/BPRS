192922800c84792c4b930844e97e9194f589b64f
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-10 10:44:05 +0200

    update: add the cache configrations for redis

--- backend/app/enum/enumerations.py
+++ backend/app/enum/enumerations.py
@@ -9,6 +9,9 @@ MAX_CONNECTIONS = int(os.getenv('MAX_CONNECTIONS', 10))
 LIMIT = int(os.getenv("LIMIT", 30))
 WINDOW_SECOND = int(os.getenv("WINDOW_SECOND", 60))
 KEY_PREFIX = os.getenv("KEY_PREFIX", "rate_limit")
+CACHE_TTL = int(os.getenv("EXPIRE", 3600))
+CACHE_PREFIX = os.getenv("CACHE_PREFIX", "search")
+CLEAR_ALL_COUNTER = int(os.getenv("CLEAR_ALL_COUNTER", 100))
 
 
 class Enumerations:
@@ -23,3 +26,8 @@ class Enumerations:
     limit = LIMIT
     window_seconds = WINDOW_SECOND
     key_prefix = KEY_PREFIX
+
+    # Caching
+    cache_ttl = CACHE_TTL
+    cache_prefix = CACHE_PREFIX
+    clear_all_counter = CLEAR_ALL_COUNTER
