0c286c4c5daf4e48c5370dcc2b1690f0b874c868
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-11 11:50:33 +0200

    refactor: add some values like (ge), (le) and update some places

--- backend/app/enum/enumerations.py
+++ backend/app/enum/enumerations.py
@@ -3,17 +3,22 @@ import os
 
 load_dotenv()
 
+KEY_PREFIX = os.getenv("KEY_PREFIX", "rate_limit")
+CACHE_PREFIX = os.getenv("CACHE_PREFIX", "search")
+
 RETRIES = int(os.getenv('RETRIES', 3))
 RETRY_DELAY = int(os.getenv('RETRY_DELAY', 3))
 MAX_CONNECTIONS = int(os.getenv('MAX_CONNECTIONS', 10))
 LIMIT = int(os.getenv("LIMIT", 30))
 WINDOW_SECOND = int(os.getenv("WINDOW_SECOND", 60))
-KEY_PREFIX = os.getenv("KEY_PREFIX", "rate_limit")
 CACHE_TTL = int(os.getenv("EXPIRE", 3600))
-CACHE_PREFIX = os.getenv("CACHE_PREFIX", "search")
 CLEAR_ALL_COUNTER = int(os.getenv("CLEAR_ALL_COUNTER", 100))
-SEARCH_REQUEST_MIN_LENGTH = int(os.getenv("SEARCH_REQUEST_MIN_LENGTH", 1))
 SEARCH_REQUEST_MAX_LENGTH = int(os.getenv("SEARCH_REQUEST_MAX_LENGTH", 255))
+SEARCH_REQUEST_MIN_LENGTH = int(os.getenv("SEARCH_REQUEST_MIN_LENGTH", 1))
+TOP_K_GREAT_THAN_OR_EQUAL = int(os.getenv("TOP_K_GREAT_THAN_OR_EQUAL", 1))
+RERANK_TOP_K_GREAT_THAN_OR_EQUAL = int(os.getenv("RERANK_TOP_K_GREAT_THAN_OR_EQUAL", 1))
+TOP_K_LESS_THAN_OR_EQUAL = int(os.getenv("TOP_K_LESS_THAN_EQUAL", 220))
+RERANK_TOP_K_LESS_THAN_OR_EQUAL = int(os.getenv("TOP_K_LESS_THAN_EQUAL", 120))
 
 
 class Enumerations:
@@ -34,6 +39,10 @@ class Enumerations:
     cache_prefix = CACHE_PREFIX
     clear_all_counter = CLEAR_ALL_COUNTER
 
-    # search request configurations (schema)
+    # schema configurations
     search_request_min_length = SEARCH_REQUEST_MIN_LENGTH
     search_request_max_length = SEARCH_REQUEST_MAX_LENGTH
+    top_k_greater_than_or_equal = TOP_K_GREAT_THAN_OR_EQUAL
+    rerank_top_k_greater_than_or_equal = RERANK_TOP_K_GREAT_THAN_OR_EQUAL
+    top_k_less_than_or_equal = TOP_K_LESS_THAN_OR_EQUAL
+    rerank_top_k_less_than_or_equal = RERANK_TOP_K_LESS_THAN_OR_EQUAL
