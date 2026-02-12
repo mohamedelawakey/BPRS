16c63fa116c3d435b344fcfc96eb10f01994ae4b
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-12 13:44:34 +0200

    refactor: integrate database pool lifecycle management (startup/shutdown)

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,21 @@ from backend.app.auth.routes import router as auth_router
 from backend.app.books.routes import router as books_router
 from backend.app.users.routes import router as users_router
 from backend.app.search.routes import router as search_router
+from backend.app.db.postgres import PostgresDBConnection
 
 app = FastAPI(title='Programming Books Recommendation System')
 
+
+@app.on_event("startup")
+async def startup_event():
+    PostgresDBConnection.init_pool()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    PostgresDBConnection.close_pool()
+
 app.include_router(auth_router, prefix='/auth', tags=['auth'])
-app.include_router(books_router, prefix='/books', tags='books')
-app.include_router(users_router, prefix='users', tags=['users'])
+app.include_router(books_router, prefix='/books', tags=['books'])
+app.include_router(users_router, prefix='/users', tags=['users'])
 app.include_router(search_router)
