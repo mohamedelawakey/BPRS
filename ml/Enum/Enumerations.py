ed081c86dab60763edc62af7430b88b65cd5a8da
mohamedelawakey <mohamedelawakey@gmail.com>
2026-02-20 15:40:37 +0200

    refactor: add the reranking weights and the min, max connections

--- ml/Enum/Enumerations.py
+++ ml/Enum/Enumerations.py
@@ -4,6 +4,14 @@ import os
 load_dotenv()
 
 EF_SEARCH = int(os.getenv('HNSW_EF_SEARCH', 120))
+RERANK_WEIGHT_SIMILARITY = float(os.getenv('RERANK_WEIGHT_SIMILARITY', 0.5))
+RERANK_WEIGHT_WEIGHTED_RATING = float(os.getenv('RERANK_WEIGHT_WEIGHTED_RATING', 0.2))
+RERANK_WEIGHT_COUNTS_OF_REVIEW = float(os.getenv('RERANK_WEIGHT_COUNTS_OF_REVIEW', 0.15))
+RERANK_WEIGHT_TECH_SCORE = float(os.getenv('RERANK_WEIGHT_TECH_SCORE', 0.05))
+RERANK_WEIGHT_PUBLISHYEAR = float(os.getenv('RERANK_WEIGHT_PUBLISHYEAR', 0.05))
+RERANK_WEIGHT_AVERAGE_LOW = float(os.getenv('RERANK_WEIGHT_AVERAGE_LOW', 0.05))
+MIN_CONNECTIONS = int(os.getenv('MIN_CONNECTIONS', 1))
+MAX_CONNECTIONS = int(os.getenv('MAX_CONNECTIONS', 20))
 
 
 class Enumerations():
@@ -40,3 +48,15 @@ class Enumerations():
     # books
     top_k = 100
     top_k_rerank = 50
+    
+    # Reranker Weights
+    weight_similarity = RERANK_WEIGHT_SIMILARITY
+    weight_weighted_rating = RERANK_WEIGHT_WEIGHTED_RATING
+    weight_counts_of_review = RERANK_WEIGHT_COUNTS_OF_REVIEW
+    weight_tech_score = RERANK_WEIGHT_TECH_SCORE
+    weight_publishyear = RERANK_WEIGHT_PUBLISHYEAR
+    weight_average_low = RERANK_WEIGHT_AVERAGE_LOW
+
+    # Connections
+    min_connections = MIN_CONNECTIONS
+    max_connections = MAX_CONNECTIONS
